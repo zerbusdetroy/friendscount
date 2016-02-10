32d0b7903b852509a30a4aa087a5492ce57d3524
Zerbus <github@zerbus.fr>
2016-02-10 22:00:44 +0100

    Initialisation du projet meteor

---
+++ client/app.ts
@@ -0,0 +1,29 @@
+
+
+import {Component, View} from 'angular2/core';
+
+ 
+
+import {bootstrap} from 'angular2/platform/browser';
+
+ 
+
+@Component({
+
+    selector: 'app'
+
+})
+
+@View({
+
+    template: 'Hello World!'
+
+})
+
+class Socially { }
+
+ 
+
+bootstrap(Socially);
+
+
